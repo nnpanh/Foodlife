6b563b6132a8b95bf7353cf18a23d1f655dd78a5
tranngoclam16 <63225101+tranngoclam16@users.noreply.github.com>
2022-07-03 00:15:41 +0700

    .

--- Foodlife/app/src/main/java/com/example/foodlife/fragments/AddRecipeFragment.kt
+++ Foodlife/app/src/main/java/com/example/foodlife/fragments/AddRecipeFragment.kt
@@ -1,26 +1,45 @@
 package com.example.foodlife.fragments
 
+import android.app.AlertDialog
+import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.foodlife.R
 import com.example.foodlife.databinding.FragmentAddrecipeTittleBinding
-import com.example.foodlife.databinding.FragmentDashboardBinding
+import com.example.foodlife.databinding.FragmentPlanBinding
 import com.example.foodlife.models.UserModel
 import com.example.foodlife.roomdb.FoodlifeDB
 import com.example.foodlife.roomdb.entities.UserEntity
-import com.example.foodlife.view_models.CollectionViewModel
+import com.example.foodlife.view_models.AddRecipeViewModel
+import com.example.foodlife.view_models.PlanViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-//import com.example.foodlife.view_models.PlanViewModel
+class AddRecipeFragment : Fragment() {
 
-class AddRecipeFragment: Fragment() {
+//    companion object {
+//        fun newInstance() = AddRecipeFragment()
+//    }
+//
+//    private lateinit var viewModel: AddRecipeViewModel
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//
+//        return inflater.inflate(R.layout.fragment_addrecipe_tittle, container, false)
+//    }
+//
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//        viewModel = ViewModelProvider(this).get(AddRecipeViewModel::class.java)
+//        // TODO: Use the ViewModel
+//    }
     private var _binding: FragmentAddrecipeTittleBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -30,25 +49,37 @@ class AddRecipeFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val collectionViewModel =
-            ViewModelProvider(this)[CollectionViewModel::class.java]
+        val planViewModel =
+            ViewModelProvider(this)[PlanViewModel::class.java]
 
         _binding = FragmentAddrecipeTittleBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-       var cover: ImageView
-       var fab: FloatingActionButton
-
-
-
+//        val textView: TextView = binding.textPlan
+//        planViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val upload: FloatingActionButton = binding.uploadimage
+        upload.setOnClickListener{
+            val builder = AlertDialog.Builder(this.context)
+            builder.setTitle("Select Image")
+            builder.setMessage("Choose your option!")
+            builder.setPositiveButton("Gallery",{dialog, which ->
+
+            })
+        }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
