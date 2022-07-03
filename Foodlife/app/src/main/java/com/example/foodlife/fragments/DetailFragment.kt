2dec0d8c82cd78993a33b11dc1a891ad13b2d9cb
nkttn72 <nkttnga@gmail.com>
2022-07-03 20:36:59 +0700

    recipe_detail

---
+++ Foodlife/app/src/main/java/com/example/foodlife/fragments/DetailFragment.kt
@@ -0,0 +1,81 @@
+package com.example.foodlife.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.MediaController
+import android.widget.TextView
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.example.foodlife.R
+import com.example.foodlife.databinding.FragmentHomeBinding
+import com.example.foodlife.view_models.HomeViewModel
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+
+class DetailFragment : Fragment() {
+
+    private lateinit var detailPagerAdapter: DetailPagerAdapter
+    private lateinit var viewPager: ViewPager2
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        detailPagerAdapter = DetailPagerAdapter(this)
+        viewPager = view.findViewById(R.id.pager)
+        viewPager.adapter = detailPagerAdapter
+
+        val tabLayout = view.findViewById<TabLayout>(R.id.tab_layout)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "OBJECT ${(position + 1)}"
+        }.attach()
+    }
+}
+
+class DetailPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = 100
+
+    override fun createFragment(position: Int): Fragment {
+        // Return a NEW fragment instance in createFragment(int)
+        val fragment = DetailObjectFragment()
+        fragment.arguments = Bundle().apply {
+            // Our object is just an integer :-P
+            putInt(ARG_OBJECT, position + 1)
+        }
+        return fragment
+    }
+}
+
+private const val ARG_OBJECT = "object"
+
+// Instances of this class are fragments representing a single
+// object in our collection.
+class DetailObjectFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
+            val textView: TextView = view.findViewById(android.R.id.text1)
+            textView.text = getInt(ARG_OBJECT).toString()
+        }
+    }
+}
