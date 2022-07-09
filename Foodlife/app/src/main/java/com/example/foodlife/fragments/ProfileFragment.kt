bcdbe20e65e2128ed2613158d8a9213710bf0027
Phương Anh <63282765+nnpanh@users.noreply.github.com>
2022-07-09 12:13:40 +0700

    hehe

--- Foodlife/app/src/main/java/com/example/foodlife/fragments/ProfileFragment.kt
+++ Foodlife/app/src/main/java/com/example/foodlife/fragments/ProfileFragment.kt
@@ -8,13 +8,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.foodlife.databinding.FragmentDetailBinding
 import com.example.foodlife.databinding.FragmentProfileBinding
 import com.example.foodlife.roomdb.FoodlifeDB
 import com.example.foodlife.view_models.ProfileViewModel
 
 class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentProfileBinding? = null
+    private var _binding: FragmentDetailBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,13 +29,9 @@ class ProfileFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this)[ProfileViewModel::class.java]
 
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textProfile
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
