0deee10996b8dae81a31892bbbbb94dc424551ed
Phương Anh <63282765+nnpanh@users.noreply.github.com>
2022-07-09 10:37:57 +0700

    finish plan flow

--- Foodlife/app/src/main/java/com/example/foodlife/dialog/CalendarPopUp.kt
+++ Foodlife/app/src/main/java/com/example/foodlife/dialog/CalendarPopUp.kt
@@ -5,21 +5,30 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.Group
 import androidx.fragment.app.DialogFragment
 import com.example.foodlife.R
 
 class CalendarPopUp : DialogFragment() {
+    private var isShowMoreInfo = false
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog{
         val dialog = Dialog(this.requireContext())
         dialog.setContentView(R.layout.dialog_calendar)
         dialog.setCancelable(true)
 
-        val image = dialog.findViewById<ImageView>(R.id.imgCalendar)
-        image.setOnClickListener {
-           image.setImageResource(R.drawable.catcool)
-        }
+        val imageInfo = dialog.findViewById<ImageView>(R.id.ivMoreInfo)
+        imageInfo.setOnClickListener {
+            val showMoreGroup = dialog.findViewById<Group>(R.id.grMoreInfo)
+            if (isShowMoreInfo){
+                showMoreGroup.visibility = View.GONE
+                isShowMoreInfo = false
+            } else {
+                showMoreGroup.visibility = View.VISIBLE
+                isShowMoreInfo = true
+            }
 
+        }
         return dialog
     }
 
