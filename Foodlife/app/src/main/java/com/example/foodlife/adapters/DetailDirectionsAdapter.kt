8cfb1c3ee85cbbdb9ed961435a6d40c668119224
nkttn72 <nkttnga@gmail.com>
2022-07-13 22:03:40 +0700

    detail directions

---
+++ Foodlife/app/src/main/java/com/example/foodlife/adapters/DetailDirectionsAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.foodlife.adapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodlife.databinding.ItemDetailDirectionsBinding
+import com.example.foodlife.databinding.ItemDetailIngredientsBinding
+import com.example.foodlife.models.DetailDirections
+
+class DetailDirectionsAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    var mList: MutableList<DetailDirections> = mutableListOf()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateData(mList: MutableList<DetailDirections>){
+        this.mList= mList
+        notifyDataSetChanged()
+    }
+
+    inner class ItemViewHolder(private val itemDetailDirectionsBinding: ItemDetailDirectionsBinding) :
+        RecyclerView.ViewHolder(itemDetailDirectionsBinding.root) {
+        fun bindData(_detailDirections: DetailDirections) {
+            itemDetailDirectionsBinding.apply {
+                detailDirections = _detailDirections
+                val connectString = "<b>${_detailDirections.bold}</b> ${_detailDirections.regular}"
+                val textValue = HtmlCompat.fromHtml(connectString, HtmlCompat.FROM_HTML_MODE_LEGACY)
+                itemDetailDirectionsBinding.tvDir.text = textValue
+                executePendingBindings()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailDirectionsAdapter.ItemViewHolder {
+        val binding =
+            ItemDetailDirectionsBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        return ItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = mList[position]
+        (holder as DetailDirectionsAdapter.ItemViewHolder).bindData(item)
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+}
--- Foodlife/app/src/main/java/com/example/foodlife/fragments/DetailDirectionsFragment.kt
+++ Foodlife/app/src/main/java/com/example/foodlife/fragments/DetailDirectionsFragment.kt
@@ -5,56 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.foodlife.R
+import com.example.foodlife.adapters.DetailDirectionsAdapter
+import com.example.foodlife.databinding.FragmentDetailDirectionsBinding
+import com.example.foodlife.models.DetailDirections
+import com.example.foodlife.view_models.DetailViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class DetailDirectionsFragment : Fragment(){
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailDirectionsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class DetailDirectionsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentDetailDirectionsBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var adapterDetailDirections: DetailDirectionsAdapter? = null
+
+    private lateinit var detailDirectionsViewModel: DetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_directions, container, false)
+        ): View? {
+
+            _binding = FragmentDetailDirectionsBinding.inflate(inflater,container,false)
+            // Inflate the layout for this fragment
+            return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailDirectionsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailDirectionsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        detailDirectionsViewModel = ViewModelProvider(this)[DetailViewModel::class.java]
+
+        initDetailIngredientsAdapter()
+    }
+
+    private fun initDetailIngredientsAdapter(){
+        adapterDetailDirections = DetailDirectionsAdapter()
+        binding.listDir.adapter = adapterDetailDirections
+        binding.listDir.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+
+        adapterDetailDirections!!.updateData(detailDirectionsViewModel.DetailDirectionsList)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
